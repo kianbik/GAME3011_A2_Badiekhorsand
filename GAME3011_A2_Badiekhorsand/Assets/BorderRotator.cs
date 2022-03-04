b5b90c588c97556dae2eefe729c7edbd67e287e6
Kian <kianbadiei@gmail.com>
2022-03-03 23:03:53 -0500

    Added SweetSpot

--- GAME3011_A2_Badiekhorsand/Assets/BorderRotator.cs
+++ GAME3011_A2_Badiekhorsand/Assets/BorderRotator.cs
@@ -6,7 +6,7 @@ public class BorderRotator : MonoBehaviour
 {
     public float rotationRate = 3.0f;
     public float timerRate = 7;
-    
+    public bool isRotating = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +17,19 @@ public class BorderRotator : MonoBehaviour
     void Update()
     {
 
-        
-        
+        RotateBoard();
+
+
     }
     public void RotateBoard()
     {
         transform.Rotate(0.0f, rotationRate * 3.0f * Time.deltaTime, 0.0f, Space.Self);
         timerRate -= Time.deltaTime;
+        isRotating = true;
+    }
+    public void StopRotateBoard()
+    {
+       
+        isRotating = false;
     }
 }
--- GAME3011_A2_Badiekhorsand/Assets/KeyMovement.cs
+++ GAME3011_A2_Badiekhorsand/Assets/KeyMovement.cs
@@ -6,6 +6,9 @@ public class KeyMovement : MonoBehaviour
 {
     public BorderRotator Board;
     public GameObject BoardStartpoint;
+    public AudioSource lockSound;
+    public int points = 0;
+    public float movementSpeed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +18,30 @@ public class KeyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
 
-            Board.RotateBoard();
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+      
+       
+        if (collision.gameObject.tag == "Win")
+        {
+            if (!lockSound.isPlaying)
+            {
+
+                lockSound.Play();
+                points++;
+            }
+                
+       
         }
-       float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-        transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
     }
 }
